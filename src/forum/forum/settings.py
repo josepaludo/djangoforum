c733d19418ed2034721bf13beb574519f678edf4
josepaludo <joseppaludo@gmail.com>
2023-05-19 12:16:19 -0300

    before railway

--- src/forum/forum/settings.py
+++ src/forum/forum/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,10 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR/'static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR/'static/',
-]
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR/"media/"
@@ -117,3 +116,7 @@ MEDIA_ROOT = BASE_DIR/"media/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Simplified static file serving.
+# https://pypi.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
